02134fc5af20c10337248a4bee0a489bbf31665a
huihui <LHGray@163.com>
2021-01-22 10:32:12 +0800

    Initial commit

---
+++ src/main/java/com/huihui/cherish/gray_dream/GrayDreamApplication.java
@@ -0,0 +1,13 @@
+package com.huihui.cherish.gray_dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayDreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrayDreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huihui/cherish/gray_dream/GrayDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huihui.cherish.gray_dream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrayDreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
